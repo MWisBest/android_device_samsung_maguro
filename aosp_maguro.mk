82223e6d5ba7a8f6651a454baf6250198b554d94
Kyle Repinski <repinski23@gmail.com>
2014-05-26 12:03:23 -0500

    Just use aosp_maguro and remove full_maguro.

--- aosp_maguro.mk
+++ aosp_maguro.mk
@@ -13,6 +13,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-$(call inherit-product, device/samsung/maguro/full_maguro.mk)
+
+#
+# This file is the build configuration for a full Android
+# build for maguro hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps). Except for a few implementation
+# details, it only fundamentally contains two inherit-product
+# lines, full and maguro, hence its name.
+#
+
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# This is where we'd set a backup provider if we had one
+#$(call inherit-product, device/sample/products/backup_overlay.mk)
+# Inherit from maguro device
+$(call inherit-product, device/samsung/maguro/device.mk)
 
 PRODUCT_NAME := aosp_maguro
+PRODUCT_DEVICE := maguro
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_RESTRICT_VENDOR_FILES := true
